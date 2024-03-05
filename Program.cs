6939e382df77fbae70e7427de0c196387723a102
Luiz <luizfernando3995@gmail.com>
2024-03-05 16:09:34 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Constantes{
+
+    static void Main(string[]args)
+    {
+        /*const string nome = "Luiz";
+        const double pi= 3.1415;
+
+        Console.WriteLine("Nome:{0}\n pi:{1} ", nome, pi); // const != variavel, pq na variavel podemos alterar esse valor, já a constante(const) não se pode fazer isso */
+
+        //Lendo Valores no teclado
+
+        /*int v1, v2,soma;
+        string nome;
+
+        Console.WriteLine("Digite o primeiro valor:"); // será o que vai aparecer no teclado
+         v1 =int.Parse (Console.ReadLine()); // le o que está vindo do teclado
+
+        Console.WriteLine("Digite o segundo valor:"); // 
+         v2 =int.Parse( Console.ReadLine()); // converte  string em int  automaticamente || ou podia ser Convert.ToInt32|| converte o que está vindo como parametro em inteiro 
+
+         soma = v1+v2;
+
+        Console.WriteLine("Resultado da soma de {0} mais {1} é igual a {2}:",v1,v2,soma); */
+
+        
+    }
+}
